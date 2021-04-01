ee00dee21e0c22499e26242214eac77d01ac587a
igor <example@gmail.com>
2021-04-01 21:38:33 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Are you here?", "Yes");
+// console.log(answer);
+
+const answers = [];
+
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('How old are you?', '');
+// answers[2] = prompt('Your surname?', '');
+
+console.log(typeof(answers));
